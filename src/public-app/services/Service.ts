b34a1ef3756737101c3b8150d612911040923d4b
juniorHotes <juniorhotes53@gmail.com>
2022-04-18 18:33:39 -0300

    New import

--- src/public-app/services/Service.ts
+++ src/public-app/services/Service.ts
@@ -1,5 +1,5 @@
-import { api } from "../../api/api";
-import { BASE_URL } from "../../api/api";
+import { api } from "./api/api";
+import { BASE_URL } from "./api/api";
 
 export class Service {
     static async getPlan() {
